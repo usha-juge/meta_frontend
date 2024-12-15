7d659e728e7932779f15639039bad25e6d5efcd7
usha-juge <juge.usha@gmail.com>
2024-12-14 21:30:31 -0800

    tab panel my card

---
+++ Module3/JavasciptProject/FirstJavascript.js
@@ -0,0 +1,19 @@
+function addTwoNum(x, y){
+    console.log(x + y);
+}
+/*
+addTwoNum(2, 3);
+addTwoNum(1, 3);
+addTwoNum(2, 2);
+
+var train = ['Wheat', 'rice'];
+console.log(train[1]);
+
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i, arr[i]); //display the array item where the index is euqal to i
+    }
+}
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
+*/
